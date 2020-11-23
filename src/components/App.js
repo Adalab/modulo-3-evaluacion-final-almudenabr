9f1384e306d219bbc7b27d89364766d0a6ae352e
Almudena <almudenablanco76@gmail.com>
2020-11-23 18:46:23 +0100

    Add html

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,36 @@
 import React from "react";
-import { Link, Route, Switch } from "react-router-dom";
+// import { Link, Route, Switch } from "react-router-dom";
 import "./App.scss";
 import Home from "./Home";
 import About from "./About";
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        Hola mundo
-        {/* <nav>
-          <ul>
-            <li>
-              {/* **** añado comp Link para convertir las rutas en enlaces */}
-        {/* <Link to="/">Home</Link> */}
-        {/* </li>
-            <li>
-              <Link to="/about">About</Link>
+const App = () => {
+  return (
+    <>
+      <header>
+        <h1>Rick and Morty</h1>
+      </header>
+
+      <main>
+        <section>
+          <form>
+            <label htmlFor="name"></label>
+            <input type="text" id="name" />
+          </form>
+        </section>
+
+        <section>
+          <ul className="listContainer">
+            <li className="cardItem">
+              <h2>
+                <p></p>
+              </h2>
             </li>
           </ul>
-        </nav>
-        <main> */}
-        {/* ****para rutas si son necesarias */}
-        {/* <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-          </Switch>
-        </main> */}
-      </>
-    );
-  }
-}
+        </section>
+      </main>
+    </>
+  );
+};
 
 export default App;
