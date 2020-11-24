08cf7bbe03bf8639249d7eb66c10c7c66c8f86b5
Almudena <almudenablanco76@gmail.com>
2020-11-24 19:16:50 +0100

    Add Link to home in CharacterDetail card

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import "./CharacterDetail.scss";
 
 const CharacterDetail = (props) => {
@@ -6,26 +7,28 @@ const CharacterDetail = (props) => {
   console.log("Detail");
   return (
     <div className="item">
-      <article>
-        <header>
-          <h2>{props.foundCharacterApp.name}</h2>
-        </header>
-        <section>
-          <ul>
-            <li>Status: {props.foundCharacterApp.status}</li>
-            <li>Species: {props.foundCharacterApp.species}</li>
-            <li>Origin: {props.foundCharacterApp.origin.name}</li>
-            <li>Episodes: 31</li>
-          </ul>
-        </section>
-        <div>
-          <img
-            className="item__img"
-            src={props.foundCharacterApp.image}
-            alt={`Imagen de ${props.foundCharacterApp.name}`}
-          />
-        </div>
-      </article>
+      <Link to="/">
+        <article>
+          <div>
+            <img
+              className="item__img"
+              src={props.foundCharacterApp.image}
+              alt={`Imagen de ${props.foundCharacterApp.name}`}
+            />
+          </div>
+          <header>
+            <h2>{props.foundCharacterApp.name}</h2>
+          </header>
+          <section>
+            <ul>
+              <li>Status: {props.foundCharacterApp.status}</li>
+              <li>Species: {props.foundCharacterApp.species}</li>
+              <li>Origin: {props.foundCharacterApp.origin.name}</li>
+              <li>Episodes: 31</li>
+            </ul>
+          </section>
+        </article>
+      </Link>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import "./Header.scss";
 
 const Header = () => {
   return (
     <header className="header">
-      <img
-        src="https://help.redbubble.com/hc/article_attachments/360002309526/Rick_and_Morty_-_logo__English_.png"
-        alt=""
-        className="header__title"
-      />
+      <Link to="/">
+        <img
+          src="https://help.redbubble.com/hc/article_attachments/360002309526/Rick_and_Morty_-_logo__English_.png"
+          alt=""
+          className="header__title"
+        />
+      </Link>
     </header>
   );
 };
